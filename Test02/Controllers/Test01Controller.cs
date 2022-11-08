3af68e3c505ada8d8f908e8546291df63e265ffc
tannha12345663 <33920360+tannha12345663@users.noreply.github.com>
2022-11-08 23:45:20 +0700

    Add project files.

---
+++ Test02/Controllers/Test01Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test02.Controllers
+{
+    public class Test01Controller : Controller
+    {
+        // GET: Test01
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test02()
+        {
+            return View();
+        }
+        public ActionResult QuanLyDL()
+        {
+            return View();
+        }
+    }
+}
