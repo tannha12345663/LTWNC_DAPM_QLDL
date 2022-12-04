9029bb903f9dc7cc10a1fa16cf71f3cb6ba7c79d
Diem Khang <diemkhang1912@gmail.com>
2022-12-04 07:51:10 +0700

    Update 4/12 - Trigger tổng tồn - page đại lý

--- Test02/Controllers/KhachHangController.cs
+++ Test02/Controllers/KhachHangController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Test02.App_Start;
+using Test02.Models;
 
 namespace Test02.Controllers
 {
+    [AuthenticationDL]
     public class KhachHangController : Controller
     {
+        QuanLyDLEntities2 db = new QuanLyDLEntities2();
         // GET: KhachHang
         public ActionResult Index()
         {
@@ -15,7 +20,56 @@ namespace Test02.Controllers
         }
         public ActionResult PageSanPham()
         {
-            return View();
+           
+            return View(db.SanPhams.ToList().OrderByDescending(s => s.TenSP));
+        }
+
+        // GET: ChiTietDonHangs
+        public ActionResult DonHangDL(string id)
+        {
+            
+            return View(db.ChiTietDonHangs.ToList());
+        }
+
+        // GET: ChiTietDonHangs/Details/5
+        public ActionResult DetailsDH(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang chiTietDonHang = db.DonHangs.Where(s => s.MaDH == id).FirstOrDefault();
+            if (chiTietDonHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chiTietDonHang);
+        }
+
+        // GET: ChiTietDonHangs/Delete/5
+        public ActionResult DeleteDH(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChiTietDonHang chiTietDonHang = db.ChiTietDonHangs.Find(id);
+            if (chiTietDonHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chiTietDonHang);
+        }
+
+        // POST: ChiTietDonHangs/Delete/5
+        [HttpPost, ActionName("DeleteDH")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteDHConfirmed(string id)
+        {
+            ChiTietDonHang chiTietDonHang = db.ChiTietDonHangs.Find(id);
+            db.ChiTietDonHangs.Remove(chiTietDonHang);
+            db.SaveChanges();
+            return RedirectToAction("DonHangDL");
         }
     }
 }
