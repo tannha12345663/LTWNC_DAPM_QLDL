5f265e2a4d5ab7211998f435b8cd8b45d1fb622d
Trương Tấn Nhã <33920360+tannha12345663@users.noreply.github.com>
2023-02-15 10:33:57 +0700

    Update Phan Quyen user

---
+++ Test02/App_Start/AdminAuthorize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Test02.App_Start
+{
+    public class AdminAuthorize:AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            //1.Check session  : Đã đăng nhập vào hệ thống = > cho thực hiện filterContext
+            //Ngược lại thì cho trở lại trang đăng nhập 
+
+            //2. Check quyền : Có quyền thì cho thực hiện filterContext
+            //Ngược lại thì cho trở lại trang đăng nhập  = > Trang từ chối truy cập
+
+        }
+    }
+}
