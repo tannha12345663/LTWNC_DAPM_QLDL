f3be9cefd5bd0050dee84412c88b63149bf89b2d
penguinDQ_247@outlook.com <penguinDQ_247@outlook.com>
2023-04-03 21:33:11 +0700

    UPDATE-QTV-DQ-4/3-9H32

--- Test02/Controllers/QTVController.cs
+++ Test02/Controllers/QTVController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,19 +23,105 @@ namespace Test02.Controllers
         {
             return View(database.NhanViens.ToList().OrderByDescending(s => s.MaNV));
         }
+        public JsonResult CheckUsernameAvailability(string userdata)
+        {
+            System.Threading.Thread.Sleep(200);
+            var SeachData =database.NhanViens.Where(x => x.UserName == userdata).SingleOrDefault();
+            
+            if (SeachData != null)
+            {
+                return Json(1);
+            }
+            else
+            {
+                return Json(0);
+            }
 
+        }
+        public JsonResult CheckEmailAvailability(string usermail)
+        {
+            System.Threading.Thread.Sleep(200);
+            var SeachData = database.NhanViens.Where(x => x.Email == usermail).SingleOrDefault();
+
+            if (SeachData != null)
+            {
+                return Json(1);
+            }
+            else
+            {
+                return Json(0);
+            }
+
+        }
+        public JsonResult CheckSDTAvailability(string userSDT)
+        {
+            System.Threading.Thread.Sleep(200);
+            var SeachData = database.NhanViens.Where(x => x.SDT == userSDT).SingleOrDefault();
+
+            if (SeachData != null)
+            {
+                return Json(1);
+            }
+            else
+            {
+                return Json(0);
+            }
+
+        }
+        public JsonResult KtraPass(string pass1,string pass2)
+        {
+            System.Threading.Thread.Sleep(200);
+            if (pass1!=pass2)
+            {
+                return Json(1);
+            }
+            else
+            {
+                return Json(0);
+            }
+
+        }
+        public void LuuAnh(NhanVien nv, HttpPostedFileBase HinhAnh)
+        {
+            #region Hình ảnh
+            //Xác định đường dẫn lưu file : Url tương đói => tuyệt đói
+            var urlTuongdoi = "/Data/Images/";
+            var urlTuyetDoi = Server.MapPath(urlTuongdoi);// Lấy đường dẫn lưu file trên server
+
+            //Check trùng tên file => Đổi tên file  = tên file cũ (ko kèm đuôi)
+            //Ảnh.jpg = > ảnh + "-" + 1 + ".jpg" => ảnh-1.jpg
+
+            string fullDuongDan = urlTuyetDoi + HinhAnh.FileName;
+            int i = 1;
+            while (System.IO.File.Exists(fullDuongDan) == true)
+            {
+                // 1. Tách tên và đuôi 
+                var ten = Path.GetFileNameWithoutExtension(HinhAnh.FileName);
+                var duoi = Path.GetExtension(HinhAnh.FileName);
+                // 2. Sử dụng biến i để chạy và cộng vào tên file mới
+                fullDuongDan = urlTuyetDoi + ten + "-" + i + duoi;
+                i++;
+                // 3. Check lại 
+            }
+            #endregion
+            //Lưu file (Kiểm tra trùng file)
+            HinhAnh.SaveAs(fullDuongDan);
+            nv.HinhAnh = urlTuongdoi + Path.GetFileName(fullDuongDan);
+        }
         public ActionResult ThemNV()
         {
+
             ViewBag.MaChucVu = new SelectList(database.ChucVus, "MaChucVu", "MaChucVu");
            
             return View();
         }
 
         [HttpPost]
-        public ActionResult ThemNV(NhanVien nhanVien)
+        public ActionResult ThemNV(NhanVien nhanVien,HttpPostedFileBase HinhAnh)
         {
             if (ModelState.IsValid)
             {
+                LuuAnh(nhanVien, HinhAnh);
                 Random rd = new Random();
                 var manv = "NV" + rd.Next(1, 1000);
                 nhanVien.MaNV = manv;
@@ -49,23 +136,28 @@ namespace Test02.Controllers
            
         }
 
-        public ActionResult ChinhsuaNV(String id)
+        public ActionResult ChinhsuaNV(String id,NhanVien nhanVien)
         {
+            ViewBag.MaChucVu = new SelectList(database.ChucVus, "MaChucVu", "MaChucVu");
             return View(database.NhanViens.Where(s => s.MaNV == id).FirstOrDefault());
         }
         [HttpPost]
-        public ActionResult ChinhsuaNV(String id,NhanVien nhanVien)
+        public ActionResult ChinhsuaNV(String id,NhanVien nhanVien,HttpPostedFileBase HinhAnh)
         {
+            
+            LuuAnh(nhanVien, HinhAnh);
             database.Entry(nhanVien).State = System.Data.Entity.EntityState.Modified;
+            ViewBag.MaChucVu = new SelectList(database.ChucVus, "MaChucVu", "MaChucVu", nhanVien.MaChucVu);
             database.SaveChanges();
             return RedirectToAction("QLNhanVien");
         }
-
-        public ActionResult XoaNV(String id)
+        public ActionResult ChitietNV(string id)
         {
-            return View(database.NhanViens.Where(s => s.MaNV == id).FirstOrDefault());
+            var nhanvien = database.NhanViens.Where(s => s.MaNV == id).FirstOrDefault();
+            return View(nhanvien);
         }
-        [HttpPost]
+        
+       
         public ActionResult XoaNV(String id,NhanVien nhanvien)
         {
             nhanvien = database.NhanViens.Where(s => s.MaNV == id).FirstOrDefault();
